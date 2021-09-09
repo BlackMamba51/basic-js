9473919fa66065fc375c024506a1590ee7fe8318
BlackMamba51 <shvyryov200@gmail.com>
2021-09-09 17:07:43 +0300

    feat: add dream-team solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,15 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function countCats( matrix ) {
+  let count = 0;
+  let len = matrix.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (matrix[i][j] == '^^') {
+        count += 1;
+      }
+    }
+  }
+  return count;
+} 
